aec7525a93e0158ed9f8c23e27b9f3dfefe87afd
jay-da846 <shuklajay2481991@gmail.com>
2026-09-10 17:55:21 +0530

    Add files via upload

---
+++ SQL_Lecture_2_CRUD_Operations prodcatdb.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE prodcatdb;
+USE prodcatdb;
+CREATE TABLE Products (
+  ProductID INT PRIMARY KEY,
+  ProductName VARCHAR(50),
+  Price DECIMAL(10,2),
+  Quantity INT
+);
+INSERT INTO Products VALUES
+(1, 'Laptop', 55000.00, 10),
+(2, 'Mobile', 15000.00, 25),
+(3, 'Tablet', 22000.00, 15),
+(4, 'Headphones', 1200.00, 50),
+(5, 'Keyboard', 800.00, 40);
+SELECT * FROM Products;
+UPDATE Products
+SET Price = 60000.00
+WHERE ProductID = 1;
+DELETE FROM Products
+WHERE ProductID = 5;
+SELECT * FROM Products
+WHERE Price > 500;
